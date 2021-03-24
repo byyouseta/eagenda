fd9e4a2835a2f3a81b82aeb97ede21d282a718f2
Bayu Sugiarno <byyouseta@gmail.com>
2021-03-24 08:13:14 +0700

    menyelesaikan model pegawai dan migrate users dan pegawai

---
+++ database/migrations/2021_03_24_005157_create_pegawais_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePegawaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->string('nama_pegawai');
+            $table->string('alamat');
+            $table->string('no_hp');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawais');
+    }
+}
